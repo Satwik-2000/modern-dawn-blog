d35d719d1545077397b876c111097c246ee92810
Satwik-2000 <satwikghosh2000@gmail.com>
2021-05-18 23:17:35 +0530

    Updates

---
+++ precache-manifest.05b9fcf126bf1e5f03590b03d64c75be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfe90f11ef6fefbe41c4c4f908af9066",
+    "url": "/modern-dawn-blog/index.html"
+  },
+  {
+    "revision": "b791aa964cc51e64a072",
+    "url": "/modern-dawn-blog/static/css/main.cfa08aa7.chunk.css"
+  },
+  {
+    "revision": "e24e547cd817c5f21146",
+    "url": "/modern-dawn-blog/static/js/2.a97780e2.chunk.js"
+  },
+  {
+    "revision": "189e23ef3562d267680aa9086e84695d",
+    "url": "/modern-dawn-blog/static/js/2.a97780e2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b791aa964cc51e64a072",
+    "url": "/modern-dawn-blog/static/js/main.74e1e487.chunk.js"
+  },
+  {
+    "revision": "9feff0fe234dddaeb90b",
+    "url": "/modern-dawn-blog/static/js/runtime-main.3ecc9647.js"
+  }
+]);
